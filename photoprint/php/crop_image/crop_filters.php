c4be18da895f70b709a5cf3bad40afcef00cd453
romanlaptev <roman@vbox5>
2019-12-10 12:29:08 +0800

    update photoprint app, php graphics

--- photoprint/php/crop_image/crop_filters.php
+++ photoprint/php/crop_image/crop_filters.php
@@ -1,4 +1,4 @@
-﻿<?php 
+<?php 
 //https://www.php.net/manual/ru/refs.utilspec.image.php
 //https://www.php.net/manual/ru/intro.image.php
 //https://www.php.net/manual/ru/function.imagecreate.php
@@ -18,8 +18,52 @@ ini_set('display_errors', 1);
 //print_r( gd_info() );
 //echo "</pre>";
 
+//https://www.php.net/manual/ru/function.get-loaded-extensions.php
+$loadedExt = get_loaded_extensions();
+if ( !in_array("gd", $loadedExt ) ) {
+	$msg = "<p>-- error, GD graphical module  is not in the list loaded extensions...</p>";
+	echo $msg;
 echo "loaded_extensions:<pre>";
-print_r( get_loaded_extensions() );
+print_r( $loadedExt );
+echo "</pre>";
+	exit;
+}
+
+if (!extension_loaded("gd") ) {
+	
+	if ( function_exists("dl") ){
+		
+		//https://www.php.net/manual/ru/function.dl.php
+		if ( dl("gd.so") ) {//try dynamic load module
+			//$msg = "<p>-- success, graphical module GD available...</p>";
+			//echo $msg;
+			runApp();
+		} else {
+			$msg = "<p>-- error, graphical module GD not loaded...</p>";
+			$msg .= "<p>-- failed load gd.so..</p>";
+			echo $msg;
+			exit;
+		}
+		
+	} else {
+		$msg = "<p>-- error, graphical module GD not loaded...</p>";
+		$msg .= "<p>-- failed load gd.so, not function dl()</p>";
+		echo $msg;
+		exit;
+	}
+	
+} else {
+	//$msg = "<p>-- success, graphical module GD available...</p>";
+	//echo $msg;
+	runApp();
+}
+
+
+function runApp(){
+/*
+//https://www.php.net/manual/ru/function.get-extension-funcs.php
+echo "extension_funcs in module GD:<pre>";
+print_r(get_extension_funcs("gd"));
 echo "</pre>";
 
 	if ( function_exists("gd_info") ){
@@ -31,43 +75,53 @@ $msg = "error, not support function gd_info(),  not  GD Support ...";
 echo $msg;
 		exit();
 	}
+*/	
+	$filename = "test.png";
+	header ("Content-type: image/png");
+	header("Content-Disposition: attachment; filename=".$filename.'');
+	header('Content-Transfer-Encoding: binary');
 
+	$im = @imagecreate (200, 200) or die ("Cannot Initialize new GD image stream");
+	$im2 = @imagecreate (200, 200) or die ("Cannot Initialize new GD image stream");
+
+	$background_color = imagecolorallocate ($im, 255, 255, 255);
+	$text_color = imagecolorallocate ($im, 233, 14, 91);
+	$color2 = imagecolorallocate ($im, 233, 14, 91);
+
+	imagestring ($im, 1, 5, 5,"Circle", $text_color);
+
+	$x0 = 100;
+	$y0 = 100;
+	$px = 60;
+	$py = 60;
+	for ($t = 0; $t < 360; $t++){
+		$x = cos($t) * $px;
+		$y = sin($t) * $py;
+		$x = $x + $x0;
+		$y = $y + $y0;
+		imagesetpixel ($im, $x, $y, $color2);
+	}//next
+
+	imagepng ($im);
+
+
+	/*
+	header("Content-Type: image/png");
+	$im = @imagecreate(110, 20) or die("Cannot Initialize new GD image stream");
+	$background_color = imagecolorallocate($im, 255, 255, 255);
+	$text_color = imagecolorallocate($im, 233, 14, 91);
+	imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
+	imagepng($im);
+	//imagedestroy($im);
+	*/
 
 /*
-header ("Content-type: image/png");
-
-$im = @imagecreate (200, 200)or die ("Cannot Initialize new GD image stream");
-$im2 = @imagecreate (200, 200)or die ("Cannot Initialize new GD image stream");
-
-$background_color = imagecolorallocate ($im, 255, 255, 255);
-$text_color = imagecolorallocate ($im, 233, 14, 91);
-$color2 = imagecolorallocate ($im, 233, 14, 91);
-
-imagestring ($im, 1, 5, 5,"Circle", $text_color);
-
-$x0 = 100;
-$y0 = 100;
-$px = 60;
-$py = 60;
-for ($t = 0; $t < 360; $t++)
-{
-	$x = cos($t) * $px;
-	$y = sin($t) * $py;
-	$x = $x + $x0;
-	$y = $y + $y0;
-	imagesetpixel ($im, $x, $y, $color2);
-}//next
-
-imagepng ($im);
+	header ("Content-type: image/png"); 
+	$im = ImageCreate (200, 100) 
+			or die ("РћС€РёР±РєР° РїСЂРё СЃРѕР·РґР°РЅРёРё РёР·РѕР±СЂР°Р¶РµРЅРёСЏ");         
+	$couleur_fond = ImageColorAllocate ($im, 255, 0, 0); 
+	ImagePng ($im); 
 */
-
-/*
-header("Content-Type: image/png");
-$im = @imagecreate(110, 20) or die("Cannot Initialize new GD image stream");
-$background_color = imagecolorallocate($im, 255, 255, 255);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
-imagepng($im);
-//imagedestroy($im);
-*/
+	
+}//end runApp()
 ?>
