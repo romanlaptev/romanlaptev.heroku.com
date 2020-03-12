28dd56414bb3679bbb6cdc956b829b62da498de2
romanlaptev <roman-laptev@yandex.ru>
2020-03-12 21:17:14 +0700

    update php code

--- projects/learn_yii/index.php
+++ projects/learn_yii/index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL|E_STRICT);
-ini_set('display_errors', 1);
+//ini_set('display_errors', 1);
+ini_set("display_errors", true);
 
 //---------------------------
 $moduleName = "sqlite3";
